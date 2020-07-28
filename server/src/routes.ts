5406d4c6433d9a87b7906f9ad2815f2a5864eb4a
Rodrigo Campos <digoleeke@gmail.com>
2020-07-28 14:02:04 -0300

    regras de validação no backend

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
+import { string } from '@hapi/joi';
 
 const routes = express.Router();
 const upload = multer(multerConfig);
@@ -13,7 +15,25 @@ const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post(
+    '/points', 
+    upload.single('image'), 
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.string().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            district: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required(),
+        })
+    }, {
+        abortEarly: false,
+    }),
+    pointsController.create
+);
  
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
